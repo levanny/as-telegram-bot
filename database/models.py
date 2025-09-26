d293505bdfe28be230451805f60f6d139e31ed69
levanny <lomidzelevani21@gmail.com>
2025-09-26 17:11:55 +0400

    created models.py wrote a db model for a car, everything toko might need.

---
+++ database/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Integer
+
+import os
+
+DATABASE_URL=os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
+
+class Car(Base):
+    __tablename__ = "cars"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    model: Mapped[str] = mapped_column(String(50))
+    year: Mapped[int] = mapped_column(Integer)
+    arrival_time: Mapped[int] = mapped_column(Integer)
+    departure_time: Mapped[int] = mapped_column(Integer)
+    price_range: Mapped[str] = mapped_column(String(50))
+    phone_number: Mapped[str] = mapped_column(String(50))
