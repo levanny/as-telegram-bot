08999c68ecba1f5cc8404dee462e9d50f38bacd9
levanny <lomidzelevani21@gmail.com>
2025-10-07 11:28:22 +0400

    created a seperate file for /list handler

---
+++ app/handlers/list_car.py
@@ -0,0 +1,37 @@
+from aiogram import Dispatcher, types
+from aiogram.filters import Command
+from sqlalchemy import select
+
+from app.database.models import Car, SessionLocal
+
+dp = Dispatcher()
+
+
+
+# List all cars
+@dp.message(Command("list"))
+async def list_cars(message: types.Message):
+    async with SessionLocal() as session:
+        result = await session.execute(select(Car))
+        cars = result.scalars().all()
+
+    if not cars:
+        await message.answer("მანქანები ჯერ არ დამატებულა!")
+        return
+
+    for car in cars:
+        car_info = (
+            f"ID: {car.id}\n"
+            f"მოდელი: {car.model}\n"
+            f"წელი: {car.year}\n"
+            f"მოყვანის თარიღი: {car.arrival_time}\n"
+            f"გატანების თარიღი: {car.departure_time}\n"
+            f"საორიენტაციო ფასი: {car.price_range}\n"
+            f"ტელეფონის ნომერი: {car.phone_number}\n"
+            "--------------------\n"
+        )
+
+        if car.photo_file_id:
+            await message.answer_photo(photo=car.photo_file_id, caption=car_info)
+        else:
+            await message.answer(car_info)
