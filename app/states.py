4d7f937c4d13549387a881e6f7e0a94d6e46ea39
levanny <lomidzelevani21@gmail.com>
2025-10-07 11:29:42 +0400

    created states.py for classes

---
+++ app/states.py
@@ -0,0 +1,20 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class DateRange(StatesGroup):
+    picking_start = State()
+    picking_end = State()
+
+class EditCarState(StatesGroup):
+    waiting_for_id = State()
+    waiting_for_field = State()
+    waiting_for_value = State()
+
+class CarState(StatesGroup):
+    id = State()
+    model = State()
+    year = State()
+    arrival_time = State()
+    departure_time = State()
+    price_range = State()
+    phone_number = State()
+    photo = State()
