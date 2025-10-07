d4cb38cad5f6cb9cb9ae2aecf0904e9feb88f2e5
levanny <lomidzelevani21@gmail.com>
2025-10-07 12:29:51 +0400

    removed unnecessary second main function, moved to aiogram v3

--- app/main.py
+++ app/main.py
@@ -1,21 +1,23 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, Router
 
 from app.config import BOT_TOKEN
 from app.handlers import help_car, add_car, edit_car, list_car
 
 logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token = BOT_TOKEN)
+dp = Dispatcher()
+
+
+
+dp.include_router(add_car.router)
+dp.include_router(list_car.router)
+dp.include_router(edit_car.router)
+
 async def main():
-    bot = Bot(token = BOT_TOKEN)
-    dp = Dispatcher()
-
-
-    dp.include_router(help_car.router)
-    dp.include_router(add_car.router)
-    dp.include_router(list_car.router)
-    dp.include_router(edit_car.router)
-
+    print("🤖 Bot is starting...")
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
